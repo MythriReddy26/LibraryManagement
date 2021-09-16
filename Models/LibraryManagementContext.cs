bd9f9c75897f8fb46d208eaf86caff14a5fc3ad1
MythriReddy26 <mythri.appidi@gmail.com>
2021-09-16 12:37:15 +0530

    Initial commit

---
+++ Models/LibraryManagementContext.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace LibraryManagement.Models
+{
+    public partial class LibraryManagementContext : DbContext
+    {
+        public LibraryManagementContext()
+        {
+        }
+
+        public LibraryManagementContext(DbContextOptions<LibraryManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Admin> Admins { get; set; }
+        public virtual DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\ProjectsV13;Database=LibraryManagement;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Admin>(entity =>
+            {
+                entity.ToTable("Admin");
+
+                entity.Property(e => e.AdminId).HasColumnName("admin_id");
+
+                entity.Property(e => e.AddAdmin).HasColumnName("add_admin");
+
+                entity.Property(e => e.Edit).HasColumnName("edit");
+
+                entity.Property(e => e.Mail)
+                    .HasMaxLength(50)
+                    .HasColumnName("mail");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .HasColumnName("password");
+            });
+
+            modelBuilder.Entity<Profile>(entity =>
+            {
+                entity.HasKey(e => e.Name)
+                    .HasName("PK__Profile__72E12F1AF0E8F3B5");
+
+                entity.ToTable("Profile");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.ConfrimPassword)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("confrim_password");
+
+                entity.Property(e => e.NewPassword)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("new_password");
+
+                entity.Property(e => e.OldPassword)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("old_password");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LibraryManagement.Models
+{
+    public partial class Profile
+    {
+        public string Name { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfrimPassword { get; set; }
+    }
+}
